56391f70c7a42faf9e5099491591f77c04a48b7c
wan iqbal <wmiqbal01@gmail.com>
2025-11-05 21:32:40 +0800

    Fix Sentry beforeSend callback typing

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/widgets.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
-import 'package:sentry/sentry.dart' as sentry;
 import 'package:freetask_app/config/app_env.dart';
 
 Future<void> bootstrap(Future<Widget> Function() builder) async {
@@ -12,11 +11,10 @@ Future<void> bootstrap(Future<Widget> Function() builder) async {
         options.dsn = AppEnv.sentryDsn;
         options.tracesSampleRate = 1.0;
         // Define with the exact typedef from package:sentry
-        final sentry.BeforeSendCallback _beforeSend =
-            (sentry.SentryEvent event, {dynamic hint}) {
-          return event; // SentryEvent? (sync) satisfies FutureOr<SentryEvent?>
-        };
-        options.beforeSend = _beforeSend;
+        SentryEvent? beforeSend(SentryEvent event, {dynamic hint}) {
+          return event;
+        }
+        options.beforeSend = beforeSend;
       },
       appRunner: () async => runApp(await builder()),
     );
